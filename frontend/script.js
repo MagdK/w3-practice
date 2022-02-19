472028d4691ab0363f91ee4597feb4a40ff2326d
MagdK <kovacsmagda@gmail.com>
2022-02-19 19:46:20 +0100

    Add comments to js

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,17 @@
-// function addTwoIntegers(firstInteger, secondInteger) {
+// Function/fuggveny, amiben ket egesz szamot osszeadunk. Function name: addTwoIntegers. parameterek: firstInteger, secondInteger. Maga az object az a kapcsos zarojele elejenel kezdodik es a vegevel vegzodik. 
+
+//  function addTwoIntegers(firstInteger, secondInteger) {
+    // a log paranccsal megmondjuk, hogy a consolba irja ki 
 //     // console.log(firstInteger + secondInteger)
+
+// A return szo a parancs vege, ezutan nem fut tovabb, akarmi kovetkezik is
 //     // return firstInteger + secondInteger
 //     // return "Hello"
+
+// if else fuggveny/function - logikat adunk hozza, a harom egyenlosegjel osszehasonlitja es meg arra is ranez, hogy mi a primitiv tipusa. 
+// Ket & jel && : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_AND
+// Logical AND (&&) evaluates operands from left to right, returning immediately with the value of the first falsy operand it encounters; if all values are truthy, the value of the last operand is returned.
+
 //     if(typeof firstInteger === "number" && secondInteger === "number"){
 //         return firstInteger + secondInteger
 //     } else{
@@ -9,15 +19,15 @@
 //     }
 // }
 
+// Mivel az egyik primitiv nem szam, igy nem fogja oket osszeadni
 // console.log( addTwoIntegers(12, "13") );
+
+// Mivel mindket primitiv szam, ezert ossze fogja oket adni. Odafenn a return-nel mar meghataroztuk, hogy mit csinaljon. MEgmondtuk neki, hogy az addTwoIntergers mit jelent
 // console.log( addTwoIntegers(1920, 85) );
 // console.log( 0 === "0" )
 
 
-
-
-
-function loadEvent(eventObject) {
+function loadEvent(eventObject){
     // console.log(eventObject)
     const roootElement = document.getElementById("root");
     // console.dir(document);
@@ -33,15 +43,16 @@ function loadEvent(eventObject) {
     // listOfSectionElements[3].classList.add("newClass");
 
 
+    // Variable-be ures stringeket is menthetunk
     let anchors = "";
 
     for (const sectionElement of listOfSectionElements) {
         // sectionElement.classList.add("newClass"); 
         // anchors = anchors + `<a> ${ sectionElement.id} </a>`;
+         // The addition assignment operator (+=) adds the value of the right operand to a variable and assigns the result to the variable. The types of the two operands determine the behavior of the addition assignment operator. Addition or concatenation is possible.
         anchors += `<a href="#${sectionElement.id}"> ${ sectionElement.id} </a>`;
     }
     console.log(anchors);
     roootElement.insertAdjacentHTML("afterbegin", `<header>${anchors}</header>`)
 }
-
 window.addEventListener("load", loadEvent)
